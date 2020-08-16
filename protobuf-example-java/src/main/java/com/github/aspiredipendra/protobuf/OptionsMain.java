fe62a1a510bf6d352f6b34ca7d85530b1e51858e
dipendra-yadav <dipendra.eng@gmail.com>
2020-08-16 13:07:26 +0530

    protocol-buffers-basics-java

---
+++ protobuf-example-java/src/main/java/com/github/aspiredipendra/protobuf/OptionsMain.java
@@ -0,0 +1,11 @@
+package com.github.aspiredipendra.protobuf;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionsMain {
+
+
+    public static void main(String[] args) {
+        OptionMessageOther other = OptionMessageOther.newBuilder().build();
+    }
+}
